1845acfd0c36f482008bdc30c89d48816f477e6c
Cody <codylau6233@hotmail.com>
2023-10-29 20:25:25 -0700

    Add taskDescription array, add new feature to newTask function that takes description of task as argument

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // Arrays to keep track of each task's state
 const taskTitles = [];
 const taskComplete = [];
+const taskDescription = [];
 
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
-function newTask(title) {
+function newTask(title, description) {
   taskTitles.push(title);
   taskComplete.push(false);
+  taskDescription.push(description);
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
