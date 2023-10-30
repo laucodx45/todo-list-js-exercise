e1790da19b44ed337bc21c64462f4d773047e021
Cody <codylau6233@hotmail.com>
2023-10-29 20:34:50 -0700

    remove completeTask and logTaskState function

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function newTask(title, description) {
   const task = {
     title: title,
@@ -6,18 +5,6 @@ function newTask(title, description) {
     complete: false
   }
   return task;
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
 }
 
 // DRIVER CODE BELOW
