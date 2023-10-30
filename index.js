be6bbe8db729fbd24d76b95cb56d06ab4fef6bba
Cody <codylau6233@hotmail.com>
2023-10-29 20:32:31 -0700

    remove arrays, newTask function now return an object

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescription = [];
 
-// Create a new task by adding to the arrays
-// A new task will be created as incomplete
 function newTask(title, description) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-  taskDescription.push(description);
+  const task = {
+    title: title,
+    description: description,
+    complete: false
+  }
+  return task;
 }
 
 // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
