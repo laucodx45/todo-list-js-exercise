bc88f09370945573b3f1cb89c17e2871509b9d38
Cody <codylau6233@hotmail.com>
2023-10-29 20:42:33 -0700

    Add logTaskState function back, remove parts of the function where it require the use of array, add new function completeTask

--- index.js
+++ index.js
@@ -5,13 +5,20 @@ function newTask(title, description) {
     complete: false
   }
   return task;
-}
+};
+
+function logTaskState(task) {
+  console.log(`${title} has${complete ? " " : " not "}been completed`);
+};
+
+function completeTask(task) {
+  task.complete = true;
+};
 
 // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box"); // task 0
+const task2 = newTask("Do Laundry", "😨");
+const task3 = [task1, task2];
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+console.log(task3);
